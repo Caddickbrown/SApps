edbcc1fa66375f2a28b4de3a6724eca696d912dc
Caddickbrown <danielcaddickbrown@gmail.com>
2020-12-19 20:02:33 +0000

    In Need of Lists

--- Algorithm_In_C++.cc
+++ Algorithm_In_C++.cc
@@ -1,19 +1,39 @@
 #include <iostream>
 using namespace std;
 #include <string>
+#include <ctime>
+#include <cstdlib>
 
 
 int main()
 {
-  int time;
-  string timeunit;
-  string verb;
-  string item;
-  string noun;
-  string nounthesecond;
-  string emotion;
+    int ln = 1;
+    int un = 60;
 
-  string timeunitarray[6]={"seconds","minutes","hours","days","weeks","months"};
+    srand((unsigned int)time(NULL));
 
-  cout << "You have " << time << timeunit << " to  " << verb << " a " << item << ". Your keywords are " << noun << nounthesecond << emotion << ". Good Luck!";
+//Time Unit
+    int vari1 = 6;
+    int RandIndex1 = rand() % vari1; //generates a random number between 0 and 5
+    string timeunitarray[vari1]={"seconds","minutes","hours","days","weeks","months"};
+//Verb
+    int vari2 = 6;
+    int RandIndex2 = rand() % vari2; //generates a random number between 0 and 5
+    string verbage[vari2]={"paint","film","draw","write","photo","record"};
+//Item
+    int vari3 = 6;
+    int RandIndex3 = rand() % vari3; //generates a random number between 0 and 5
+    string itemarray[vari3]={"seconds","minutes","hours","days","weeks","months"};
+//Nouns
+    int vari4 = 6;
+    int RandIndex4 = rand() % vari4; //generates a random number between 0 and 5
+    string nounarray[vari4]={"paint","film","draw","write","photo","record"};
+//Emotion
+    int vari5 = 6;
+    int RandIndex6 = rand() % vari5; //generates a random number between 0 and 5
+    string emotionarray[vari5]={"paint","film","draw","write","photo","record"};
+
+//Print
+    cout<<"You have " << ln+rand() % static_cast<int>(un-ln+1) << " " << timeunitarray[RandIndex1] << " to " << verbage[RandIndex2] << " a " << itemarray[RandIndex3] << ". Your keywords are " << nounarray[RandIndex4]  << " " << nounarray[RandIndex4]  << " " << "emotion" << ". Good Luck!";
+    return 0;
 }
