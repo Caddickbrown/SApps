db39e91f0e4a8e5c76597b7b7d779ecfb6d10465
Caddickbrown <danielcaddickbrown@gmail.com>
2022-07-29 13:38:40 +0100

    Shuffle

--- JSONalgorithm.js
+++ JSONalgorithm.js
@@ -1,12 +1,21 @@
 // Declare variables
-var timeunit = ; //Units of Time
-var nouns = ; //Nouns
+var timeunit = ; //Time Unit
 var verbage = ; //Verbs
 var item = ; //Items
+var nouns = ; //Nouns
 var emotion = ; //Emotions
 var ln = 1; //Lower limit for time
 var un= 60; //Upper limit for time
 
+//Randomiser
+/*var array = [
+    ["Mango","M"],
+    ["Lychee","L"],
+    ["Pineapple","P"],
+    ["Banana","B"]
+];
+var randomItem = array[Math.random() * array.length | 0];
+*/
 
-
+//Print
 document.write("You have ",ln+rand() % static_cast<int>(un-ln+1)," ",timeunitarray[RandIndex1]," to ",verbage[RandIndex2]," ",itemarray[RandIndex3],". Your keywords are ",nouns,[RandIndex4],", ",nouns[RandIndex5],", and ",emotion[RandIndex6],". Good Luck!";)
