48103cf7f53a8de270d9d713cee68db6ea475299
Caddickbrown <danielcaddickbrown@gmail.com>
2022-12-19 21:51:16 +0000

    I hate Javascript

--- JavaScript/JSAlgorithmix.js
+++ JavaScript/JSAlgorithmix.js
@@ -3,7 +3,7 @@ var timeunit = 1; //Time Unit
 var verbage = 2; //Verbs
 var item = 3; //Items
 var nouns = 4; //Nouns
-var emotion = 5; //Emotions
+var emotion = "Pringle"; //Emotions
 var ln = 1; //Lower limit for time
 var un= 60; //Upper limit for time
 
@@ -17,8 +17,20 @@ var un= 60; //Upper limit for time
 var randomItem = array[Math.random() * array.length | 0];
 */
 
+const greetings = ['Howdy', 'Bonjour', 'Hello', 'Dude']
+
+const greeting = greetings[  
+  Math.floor(Math.random() * greetings.length)
+]
+
+var x = "Hello ";
+var y = "World!";
+
+
+
+
 //Print
-document.write("You have ",ln+rand() % static_cast<int>(un-ln+1)," ",timeunitarray[RandIndex1]," to ",verbage[RandIndex2]," ",itemarray[RandIndex3],". Your keywords are ",nouns,[RandIndex4],", ",nouns[RandIndex5],", and ",emotion[RandIndex6],". Good Luck!");
+//const x = `hello ${emotion}`;
 
 // Import
 // Setup
@@ -26,3 +38,49 @@ document.write("You have ",ln+rand() % static_cast<int>(un-ln+1)," ",timeunitarr
 // Pull Data and Assign Index Numbers
 // Generate Words
 // Print
+
+
+
+
+var jsonContent = {
+  "featured": [
+      {
+          "id": "111",
+          "title": "post title 111",
+          "desc": "This is a test desc 111"
+      },
+      {
+          "id": "222",
+          "title": "post title 222",
+          "desc": "This is a test desc 222"
+      },
+      {
+          "id": "333",
+          "title": "post title 333",
+          "desc": "This is a test desc 333"
+      }
+  ]
+}
+
+var random = jsonContent.featured[Math.floor(Math.random() * jsonContent.featured.length)];
+console.log(random)
+
+var random = jsonContent["featured"][Math.floor(Math.random()*jsonContent["featured"].length)];
+
+
+var rawdata = fs.readFileSync('/../list.json');
+  var jsonlist = JSON.parse(rawdata);
+
+  var randomNum = Math.round(Math.random() * (1 + 1169) - 1);
+
+  var pokemon = pokemon2[randomNum];
+
+  console.log(pokemon);
+  console.log(typeof(pokemon));
+
+  res.json(pokemon);
+
+  var json = JSON.stringify(arr);
+alert(json);
+
+//const x = console.log(jsonlist["timeunit"][Math.floor(Math.random()*jsonContent["featured"].length)]);
